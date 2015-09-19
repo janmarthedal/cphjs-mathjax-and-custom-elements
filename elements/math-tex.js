b324940527e1d1e8b9058ce5244ed5645238fd4b
Jan Marthedal Rasmussen <jan@janmr.com>
2015-09-19 20:31:27 +0200

    Mutation observer, queueing

--- elements/math-tex.js
+++ elements/math-tex.js
@@ -1,13 +1,66 @@
 (function() {
 
-  var math_tex_prototype = Object.create(HTMLElement.prototype);
+    var TAG_NAME = 'math-tex',
+        HANDLER_TAG_NAME = 'mathjax-loader',
+        mutation_config = {childList: true, characterData: true, attributes: true},
+        handler,
+        element_prototype = Object.create(HTMLElement.prototype);
 
-  math_tex_prototype.createdCallback = function() {
-    this.textContent = "I'm a math-tex";
-  };
+    function check_handler() {
+        if (handler) return true;
+        handler = document.querySelector(HANDLER_TAG_NAME);
+        if (handler) return true;
+        handler = document.createElement(HANDLER_TAG_NAME);
+        if (handler && typeof handler.typeset === 'function') {
+            document.head.appendChild(handler);
+            return true;
+        }
+        console.warn(['no', HANDLER_TAG_NAME, 'element defined;', TAG_NAME, 'element will not work'].join(' '));
+        handler = undefined;
+        return false;
+    }
 
-  document.registerElement('math-tex', {
-    prototype: math_tex_prototype
-  });
+    function update(elem) {
+        var jax = elem._private.jax,
+            firstTypeset = !jax.hasAttribute('type');
+        jax.type = elem.getAttribute('display') === 'block' ? 'math/tex; mode=display' : 'math/tex';
+        jax.textContent = elem.textContent;
+        firstTypeset ? handler.typeset(jax) : handler.reprocess(jax);
+    }
+
+    element_prototype.createdCallback = function () {
+        var sdom = this.createShadowRoot();
+        var script = document.createElement('script');
+        sdom.appendChild(script);
+        this._private = {jax: script};
+    };
+
+    element_prototype.attachedCallback = function () {
+        if (check_handler()) {
+            var elem = this;
+            if (this.textContent.trim())
+                update(this);
+            this._private.observer = new MutationObserver(function () {
+                update(elem);
+            });
+            this._private.observer.observe(this, mutation_config);
+        }
+    };
+
+    element_prototype.detachedCallback = function () {
+        if (this._private) {
+            this._private.observer.disconnect();
+            delete this._private;
+        }
+    }
+
+    /*element_prototype.attributeChangedCallback = function (attr, oldVal, newVal) {
+        if (attr === 'display')
+            update(this._private.jax, newVal);
+    };*/
+
+    document.registerElement(TAG_NAME, {
+        prototype: element_prototype
+    });
 
 })();
--- elements/mathjax-loader.js
+++ elements/mathjax-loader.js
@@ -1,13 +1,74 @@
 (function() {
 
-  var mathjax_loader_prototype = Object.create(HTMLElement.prototype);
+    var states = {start: 1, configured: 2, loading: 3, ready: 4, error: 5},
+        state = states.start,
+        typeset_queue = [],
+        reprocess_queue = [],
+        typesetting = false,
+        element_prototype = Object.create(HTMLElement.prototype),
+        options = {
+            src: "http://cdn.mathjax.org/mathjax/latest/MathJax.js",
+            async: true
+        };
 
-  mathjax_loader_prototype.createdCallback = function() {
-    this.textContent = "I'm a mathjax-loader";
-  };
+    function start() {
+        if (typeset_queue.length) {
+            var q = typeset_queue;
+            typeset_queue = [];
+            console.log('Typesetting ' + q.length);
+            MathJax.Hub.Queue(['Typeset', MathJax.Hub, q, start]);
+        } else if (reprocess_queue.length) {
+            var q = reprocess_queue;
+            reprocess_queue = [];
+            console.log('Reprocessing ' + q.length);
+            MathJax.Hub.Queue(['Reprocess', MathJax.Hub, q, start]);
+        } else
+            typesetting = false;
+    }
 
-  document.registerElement('mathjax-loader', {
-    prototype: mathjax_loader_prototype
-  });
+    function check_queue() {
+        if (state === states.ready && !typesetting) {
+            typesetting = true;
+            start();
+        }
+    }
+
+    function readyCallback() {
+        state = states.ready;
+        check_queue();
+    }
+
+    element_prototype.attachedCallback = function () {
+        MathJax = {
+            skipStartupTypeset: true,
+            jax: ["input/TeX", "output/HTML-CSS"],
+            AuthorInit: function () {
+                MathJax.Hub.Register.StartupHook("End Config", function () {
+                    var waitFor = MathJax.Hub.config.skipStartupTypeset ? "End" : "Begin Typeset";
+                    MathJax.Hub.Register.StartupHook(waitFor, readyCallback);
+                });
+            }
+        };
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = options.src;
+        script.async = options.async;
+        script.onerror = function () { state = states.error; queue = []; };
+        document.head.appendChild(script);
+    };
+
+    element_prototype.typeset = function (elem) {
+        typeset_queue.push(elem);
+        check_queue();
+    };
+
+    element_prototype.reprocess = function (elem) {
+        reprocess_queue.push(elem);
+        check_queue();
+    };
+
+    document.registerElement('mathjax-loader', {
+        prototype: element_prototype
+    });
 
 })();
